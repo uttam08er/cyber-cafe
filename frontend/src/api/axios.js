f6b87b0f63d87482036b88de8594069d4524acc1
Uttam Kumar <uttamkssm2000.com>
2026-06-20 13:35:53 +0530

    Initial commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000" || 'http://10.163.2.66:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
